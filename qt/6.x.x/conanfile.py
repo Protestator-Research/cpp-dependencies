f4a04a3d5d5d0f7ee296413bf1df1ff5617220a5
Moritz Herzog <TheMHMoritz3@users.noreply.github.com>
2025-01-03 22:30:26 +0100

    Working on QT Build and adding Linux headers

--- qt/6.x.x/conanfile.py
+++ qt/6.x.x/conanfile.py
@@ -389,9 +389,9 @@ class QtConan(ConanFile):
             else:
                 self.requires("libjpeg/9e")
         if self.options.get_safe("with_libpng", False) and not self.options.multiconfiguration:
-            self.requires("libpng/[>=1.6 <2]")
+            self.requires("libpng/1.6.42")
         if self.options.with_sqlite3 and not self.options.multiconfiguration:
-            self.requires("sqlite3/[>=3.45.0 <4]")
+            self.requires("sqlite3/3.45.0")
         if self.options.get_safe("with_mysql", False):
             self.requires("libmysqlclient/8.1.0")
         if self.options.with_pq:
